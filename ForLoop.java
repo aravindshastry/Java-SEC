a24de0a17a3691cef186bed7be21c6edaeb78d61
Aravind Shastry <shastryaravind55@gmail.com>
2026-08-17 22:34:34 +0530

    Organize Java programs

--- ForLoop.java
+++
@@ -1,9 +0,0 @@
-// Print all numbers from 1 to 10.
-
-public class ForLoop {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(i);
-        }
-    }
-}
--- Java SEC/ForLoop.java
+++ Java SEC/ForLoop.java
@@ -1,3 +1,5 @@
+// Print all numbers from 1 to 10.
+
 public class ForLoop {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
