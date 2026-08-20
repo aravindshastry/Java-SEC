8b04aff947f7ea0cc843404bbb686ebefd8be10b
Aravind Shastry <shastryaravind55@gmail.com>
2026-08-20 14:11:52 +0530

    Add new Java program

---
+++ Java SEC/Array.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Your array:");
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
